6268639b346387afbb9748c3ec43a4e6cb41c6b0
strbova43 <strbova43@me>
2015-09-21 11:27:02 +0200

    nova verzia

--- hanojske_veze.cpp
+++ hanojske_veze.cpp
@@ -1,25 +1,15 @@
 #include <iostream.h>
 #include <conio.h>
 
-<<<<<<< HEAD
 void veza(int a,char from,char aux,char to){
-=======
-void hanoi(int a,char from,char aux,char to){
->>>>>>> b69e77f8a133f4d79454984c21d09d6a65fa9545
     if(a==1){
        cout<<"\t\tPresun disk 1 z "<<from<<" do "<<to<<"\n";
        return;
     }
     else{
-<<<<<<< HEAD
        veza(a-1,from,to,aux);
        cout<<"\t\tPresun disk "<<a<<" z "<<from<<" do "<<to<<"\n";
        veza(a-1,aux,from,to);
-=======
-       hanoi(a-1,from,to,aux);
-       cout<<"\t\tPresun disk "<<a<<" z "<<from<<" do "<<to<<"\n";
-       hanoi(a-1,aux,from,to);
->>>>>>> b69e77f8a133f4d79454984c21d09d6a65fa9545
     }
 }
 
@@ -30,6 +20,6 @@ void main(){
      cout<<"\t\tVloz poet diskov : ";
      cin>>n;
      cout<<"\n\n";
-     veza(n,'A','B','C');
+     tower(n,'A','B','C');
      getch();
 }
